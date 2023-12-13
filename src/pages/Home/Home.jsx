aa925a465c3c01d9bd13b8ac42ed270502f50f87
Julio Bermudez <julioph0n3@gmail.com>
2023-12-13 05:28:08 -0500

    added footer component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import AboutUs from "../../components/AboutUs/AboutUs"
+import AppSection from "../../components/AppSection/AppSection"
 import CategoryShowcase from "../../components/CategoryShowcase/CategoryShowcase"
 import Banner from "../../components/Header/Banner"
 import HomeCategory from "../../components/HomeCategory/HomeCategory"
 import LocationSprade from "../../components/LocationSprade/LocationSprade"
 import Register from "../../components/Register/Register"
+import Sponsor from "../../components/Sponsor/Sponsor"
 
 const Home = () => {
   return (
@@ -14,6 +16,8 @@ const Home = () => {
     <Register/>
     <LocationSprade/>
     <AboutUs/>
+    <AppSection/>
+    <Sponsor/>
     </>
   )
 }
