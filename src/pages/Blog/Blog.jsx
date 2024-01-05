3c9c58093b34bcf0b827744334409b9a9aabffe5
Julio Bermudez <julioph0n3@gmail.com>
2024-01-04 22:04:01 -0500

    added payment method but need full funtionability

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,12 +1,49 @@
-import PageHeader from "../../components/PageHeader/PageHeader"
+import PageHeader from "../../components/PageHeader/PageHeader";
+import blogList from "../../utilis/blogdata";
+import { Link } from "react-router-dom";
 
-const title ="Blog Page"
-const currentPage = "Blogs"
+const title = "Blog Page";
+const currentPage = "Blogs";
 
 const Blog = () => {
   return (
-    <PageHeader title = {title} currentPage={currentPage}/>
-  )
-}
+    <div>
+      <PageHeader title={title} currentPage={currentPage} />
+      <div className="blog-section padding-tb section-bg">
+        <div className="container">
+          <div className="section-wrapper">
+            <div className="row row-cols-1 row-cols-md-2 row-cols-xl-3 justify-content-center g-4">
+              {blogList.map((blog, i) => (
+                <div key={i} className="col">
+                  <div className="post-item">
+                    <div className="post-inner">
+                      <div className="post-thumb">
+                        <Link to={`/blog/${blog.id}`}>
+                          <img src={blog.imgUrl} alt="" />
+                        </Link>
+                      </div>
+                      <div className="post-content">
+                        <Link to={`/blog/${blog.id}`}>
+                          <h4>{blog.title}</h4>
+                        </Link>
+                        <div className="meta-post">
+                          <ul className="lab-ul">
+                            {
+                              
+                            }
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Blog
+export default Blog;
